f855b26c47d173822ea6e152ba4af859aaebf4d2
qiuwei <qiuwei@fullgoalos.com.cn>
2021-12-03 10:40:23 +0800

    添加 docusaurus 学习笔记: Guides - Pages

---
+++ src/pages/HelloReact.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+import Layout from '@theme/Layout'
+
+const HelloReact: FC = () => {
+  return (
+    <Layout title="Hello React">
+      <div>Hello React!</div>
+    </Layout>
+  )
+}
+
+export default HelloReact
